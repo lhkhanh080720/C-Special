c4573550bf7604f151541be25bdcc6a08a0fa6ed
lhkhanh080720 <lehuukhanh872000@gmail.com>
2024-03-15 23:50:53 +0700

    init

--- Sp1.c
+++ Sp1.c
@@ -3,7 +3,13 @@
 int main()
 {
     int a = 11, b = 11;
-    printf("Hello World\n");
-    printf("%d %d %d %d", ++a, a++, b++, ++b);
+    printf("%d %d\n", a++, a++);    
+    printf("%d\n", a);
+    printf("%d\n", ++a);
+    a = 10; 
+    printf("%d %d %d %d\n", a, a++, ++a, a++); 
+    a = 10;
+    b = 11; 
+    printf("%d %d %d %d\n", ++a, a++, b++, ++b);    
     return 0;
 }
